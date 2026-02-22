2b4d11c8806a52ca9680ee194a65739f4a805e58
Nick van Vliet <nvliet@proton.me>
2026-02-22 23:49:32 +0100

    initial commit

---
+++ app.py
@@ -0,0 +1,46 @@
+from typing import Annotated
+from pydantic import BaseModel
+from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
+
+import database
+from apikey import API_KEY
+
+app = FastAPI()
+
+security = HTTPBearer()
+
+TOKEN = API_KEY
+
+class Item(BaseModel):
+    km: float
+    liters: float
+
+class Afrekenen(BaseModel):
+    km: float
+
+@app.post("/tanken")
+def tanken(item: Item,
+    credentials: Annotated[HTTPAuthorizationCredentials, Depends(security)]
+):
+    if credentials.credentials != TOKEN:
+        raise HTTPException(status_code=401, detail="Invalid API Key")
+    else:
+        con, cur = database.check_and_makedb() 
+        database.insert_data(con=con,cur=cur, km=item.km, liters=item.liters)
+        con.close()
+        return f"Updated: {item}"
+
+@app.post("/afrekening")
+def afrekening(afrekenen: Afrekenen,
+    credentials: Annotated[HTTPAuthorizationCredentials, Depends(security)]
+):
+    if credentials.credentials != TOKEN:
+        raise HTTPException(status_code=401, detail="Invalid API Key")
+    else:
+        con, cur = database.check_and_makedb() 
+        avg_km, avg_liters = database.get_averages(con, cur)
+        avg_consumption = avg_liters/avg_km
+
+        est_liters = avg_consumption * afrekenen.km
+        return est_liters
---
+++ database.py
@@ -0,0 +1,43 @@
+import sqlite3
+import pathlib
+
+def check_and_makedb() -> [sqlite3.Connection, sqlite3.Cursor]:
+    if pathlib.Path("data.db").is_file():
+        con = sqlite3.connect("data.db")
+        cur = con.cursor()
+        return con, cur
+    else:
+        con = sqlite3.connect("data.db")
+        cur = con.cursor()
+        cur.execute("CREATE TABLE transacties(km, liters)")
+        return con, cur
+
+def insert_data(con:sqlite3.connect, cur:sqlite3.Cursor, km:float, liters:float):
+    data = (km, liters)
+    cur.execute("INSERT INTO transacties VALUES(?, ?)", data)
+    con.commit()
+
+def get_averages(con:sqlite3.connect, cur:sqlite3.Cursor) -> tuple[float,float]:
+    res = cur.execute("SELECT AVG(km), AVG(liters) FROM transacties")
+    avg_km, avg_liters = res.fetchone()
+    return avg_km, avg_liters
+
+
+def get_prijs():
+  data = requests.get("https://opendata.cbs.nl/ODataApi/odata/80416ned/TypedDataSet").json()
+  df = pd.DataFrame(data["value"])
+  prijs = df.iloc[-1,2]
+
+  current_dir = pathlib.Path.cwd()
+  root = tk.Tk()
+  root.tk.call('tk', 'scaling', 1.0)
+  root.withdraw()
+  pad = filedialog.askopenfilename(initialdir=current_dir, filetypes=[("Excel files", ".xlsx .xls")], title="Selecteer excel bestand met de ritten")
+  root.destroy()
+  verbruik = gem_verbruik()
+  ritten = get_ritten(pad)
+
+  res = verbruik*ritten*prijs
+  res = round(res, 2)
+
+  return res
